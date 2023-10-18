7de7a2ce4f50d2a915dd8bc2334aa1d7f21be106
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-10-18 15:32:25 +0200

    ajout du styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,30 @@
-function createCard() {
-    let name = document.getElementById("name").value;
-    let job = document.getElementById("job").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
+function validerFormulaire() {
+    const nom = document.getElementById('nom').value;
+    const entreprise = document.getElementById('entreprise').value;
+    const email = document.getElementById('email').value;
+    const telephone = document.getElementById('telephone').value;
+    const card = document.getElementById('formulaire')
+
+    if (nom && entreprise && email && telephone) {
+      afficherCarteVisite(nom, entreprise, email, telephone);
+      card.style.display = 'none'
+    } else {
+      alert('Veuillez remplir tous les champs.');
+    }
+  }
   
-    let card = document.getElementById("card");
-    card.innerHTML = "<h2>" + name + "</h2>" +
-                     "<p><strong>Profession :</strong> " + job + "</p>" +
-                     "<p><strong>Email :</strong> " + email + "</p>" +
-                     "<p><strong>Téléphone :</strong> " + phone + "</p>";
   
-    card.style.display = "block";
+  function afficherCarteVisite(nom, entreprise, email, telephone) {
+    const carteVisite = document.getElementById('carteVisite');
+    const nomAffiche = document.getElementById('nomAffiche');
+    const entrepriseAffiche = document.getElementById('entrepriseAffiche');
+    const emailAffiche = document.getElementById('emailAffiche');
+    const telephoneAffiche = document.getElementById('telephoneAffiche');
+  
+    nomAffiche.textContent = nom;
+    entrepriseAffiche.textContent = entreprise;
+    emailAffiche.textContent = email;
+    telephoneAffiche.textContent = telephone;
+    
+    carteVisite.classList.remove('hidden');
   }
